2cfc4a9a29a0c195c49f4f52973cf8f1f8efd751
Sead Šijerkić <sead_sijerkic@hotmail.com>
2024-07-26 11:43:20 +0200

    server js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv").config();
+const axios = require("axios");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+// API request
+app.get("/", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://fir-design-visitors-default-rtdb.europe-west1.firebasedatabase.app",
+    headers: {
+      "X-RapidAPI-Key": process.env.FIREBASE_API_KEY,
+      "X-RapidAPI-Host": "fir-design-visitors.firebaseapp.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,28 @@ import { Container } from '@mui/material';
 import HeroLandingPage from '@/sections/HeroLandingPage';
 import TrustedBy from '@/sections/TrustedBy';
 import SocialMediaPostExamples from '@/sections/SocialMediaPostExamples';
+import axios from "axios";
+import { useState, useEffect } from "react"
 
 const Home: React.FC = () => {
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      url: "http://localhost:3000",
+    }
+    axios.request(options)
+      .then(function (response) {
+        setData(response.data.data)
+      })
+      .catch(function (error) {
+        console.error(error);
+      })
+  }, [])
+
+  console.log(data)
   return (
     <MainLayout>
       {/* <Container maxWidth='lg'> */}
