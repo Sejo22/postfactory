ace5ad239d5916b88a399ddaaeebebbc393f38fc
Sead Šijerkić <sead_sijerkic@hotmail.com>
2024-10-12 23:53:47 +0200

    fixed build issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import HeroLandingPage from '@/sections/HeroLandingPage';
 import TrustedBy from '@/sections/TrustedBy';
 import SocialMediaPostExamples from '@/sections/SocialMediaPostExamples';
 import Plans from '@/sections/Plans';
-import Services from '@/sections/Services';
+// import Services from '@/sections/Services';
 import FaqLandingPage from '@/sections/FaqLandingPage';
 import CTA from '@/components/CTA';
 import Reviews from '@/sections/Reviews';
